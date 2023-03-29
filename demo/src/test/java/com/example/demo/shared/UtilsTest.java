f2fc1438ac7f9bc097364d58f9aad3d76996f55c
Toma <tomaandrei98@gmail.com>
2023-03-29 16:47:33 +0300

    Add test for generateUserId from Utils class

---
+++ demo/src/test/java/com/example/demo/shared/UtilsTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.shared;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UtilsTest {
+
+    @Autowired
+    Utils utils;
+
+    @Test
+    public void testGenerateUserId() {
+        String userId = utils.generateUserId(30);
+        String userId2 = utils.generateUserId(30);
+
+        assertNotNull(userId);
+        assertNotNull(userId2);
+
+        assertTrue(userId.length() == 30);
+        assertTrue(userId2.length() == 30);
+
+        assertTrue(!userId.equalsIgnoreCase(userId2));
+    }
+}
